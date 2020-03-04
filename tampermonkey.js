b17228d7d0941c8370c10f7dbb4f01720a06bfe4
kpkym <okpkym@gmail.com>
2020-03-04 21:25:00 +0800

    修复楼中楼语音条问题

--- tampermonkey.js
+++ tampermonkey.js
@@ -3,19 +3,29 @@ async function getMobileData(){
     let pcData;
     let mobileData;
     mobileHTML.innerHTML = await fetch("http://localhost:14623" + location.pathname).then(e => e.text());
-    pcData = document.querySelectorAll(".voice_player_inner")
-    mobileData = mobileHTML.querySelectorAll(".j_voice_wrap")
+    mobileData = mobileHTML.querySelectorAll("li[tid] .list_main");
+    pcData = document.querySelectorAll("[data-field] .d_post_content_main");
 
     if (pcData.length !== mobileData.length){
-        alert("PC跟移动端语音条数据不一致，可能是因为楼中楼")
+        alert("PC跟移动端楼层不一致")
     }
 
-    pcData.forEach((e, index) => {
-        let newNode = document.createElement("html");
-        newNode.innerHTML = `<audio src="${mobileData[index].dataset["url"]}" controls="controls"></audio>`
-        e.parentElement.replaceChild(newNode, e);
-    });
-};
+    mobileData.forEach((e, index) => {
+        if (e.querySelector(".content .j_voice_wrap")){
+            let newNode = document.createElement("html");
+            newNode.innerHTML = `<audio src="${e.querySelector(".content .j_voice_wrap").dataset.url}" controls="controls"></audio>`
+            let voiceEle = pcData[index].querySelector(".p_content .voice_player a");
+            voiceEle.parentElement.replaceChild(newNode, voiceEle);
+        }
+        if (e.querySelector(".j_floor_panel .j_voice_wrap")){
+            let newNode = document.createElement("html");
+            newNode.innerHTML = `<audio src="${e.querySelector(".j_floor_panel .j_voice_wrap").dataset.url}" controls="controls"></audio>`
+            let voiceEle = pcData[index].querySelector(".core_reply .voice_player_inner");
+            voiceEle.parentElement.replaceChild(newNode, voiceEle);
+            }
+        }
+    );
+}
 
 getMobileData();
 

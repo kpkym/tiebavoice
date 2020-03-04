96fa4ed0b36991e1ae126b037a61ff8bff698fe3
kpkym <okpkym@gmail.com>
2020-03-05 00:35:12 +0800

    楼中楼语音替换报错修改

--- tampermonkey.js
+++ tampermonkey.js
@@ -14,20 +14,31 @@ async function getMobileData(){
         if (e.querySelector(".content .j_voice_wrap")){
             let newNode = document.createElement("html");
             newNode.innerHTML = `<audio src="${e.querySelector(".content .j_voice_wrap").dataset.url}" controls="controls"></audio>`
-            let voiceEle = pcData[index].querySelector(".p_content .voice_player a");
+            let voiceEle = pcData[index].querySelector(".p_content .voice_player a");            
             voiceEle.parentElement.replaceChild(newNode, voiceEle);
         }
         if (e.querySelector(".j_floor_panel .j_voice_wrap")){
             let newNode = document.createElement("html");
             newNode.innerHTML = `<audio src="${e.querySelector(".j_floor_panel .j_voice_wrap").dataset.url}" controls="controls"></audio>`
-            let voiceEle = pcData[index].querySelector(".core_reply .voice_player_inner");
-            voiceEle.parentElement.replaceChild(newNode, voiceEle);
-            }
+            
+            let btn = document.createElement("button");
+            btn.appendChild(document.createTextNode("替换楼中楼语音条"));
+            btn.addEventListener("click", () => {
+                let voiceEle = pcData[index].querySelector(".core_reply .voice_player_inner");
+                voiceEle.parentElement.replaceChild(newNode, voiceEle);
+            });
+            pcData[index].querySelector(".p_content").appendChild(btn);
         }
-    );
+    });
+
+    let btn = document.createElement("button");
+    btn.appendChild(document.createTextNode("替换楼中楼语音条"));
+    btn.addEventListener("click", () => funcList.forEach(e => e()));
+    document.querySelector("#pager_go6").parentElement.append(btn);
 }
 
 getMobileData();
+
 
 // 显示移动端隐藏数据
 // document.querySelectorAll(".class_hide_flag").forEach(e=>e.classList.remove("class_hide_flag"));
